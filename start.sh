5f63babacab27b25591bdbd3cebd012a3e949530
Matwa <mateowatata@gmail.com>
2026-02-28 01:52:01 -0500

    apparently systemd isn't picking up the env var

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # WebSocket URL - can be overridden via environment
-export WEBSOCKET_URL="${WEBSOCKET_URL:-ws://localhost:8080}"
+export WEBSOCKET_URL="${WEBSOCKET_URL:-ws://matwa.is-cool.dev/ws/}"
 
 npm i
 
